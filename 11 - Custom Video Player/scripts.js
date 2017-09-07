ee43e0f0086c1a5c882fe8a6d17f92e0287733f9
Daniel Rodriguez <drod2793@gmail.com>
2017-09-06 19:53:37 -0600

    11: adds updating video time based on updates to progress bar

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,10 +5,12 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+let mousedown = false;
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(function(skipButton) {
   skipButton.addEventListener('click', skip);
@@ -17,6 +19,10 @@ ranges.forEach(function(range) {
   range.addEventListener('change', handleRangeUpdate);
   range.addEventListener('mousemove', handleRangeUpdate);
 })
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
 
 function togglePlay() {
   video.paused ? video.play() : video.pause();
@@ -33,4 +39,14 @@ function skip() {
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
